8a68ed1fb06896b92179e9c6c72068a83055d557
Adam Zieman <adamzieman@yahoo.com>
2024-02-06 23:46:20 -0600

    Restructures file

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from gui.ui_main_window import *
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    
     MainWindow.show()
     sys.exit(app.exec_())
