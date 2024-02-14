aee6047d8a3f7531bbd95b3d861a113f6164cff8
Adam Zieman <adamzieman@yahoo.com>
2024-02-14 03:15:26 -0600

    Reduces PyQt5 import size
    Only imports necessary PyQt5 methods and removes QtCore / QtGui / GtWidgets from every line of code that calls it to increase readability.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from ui_main_window import *
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
 
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
